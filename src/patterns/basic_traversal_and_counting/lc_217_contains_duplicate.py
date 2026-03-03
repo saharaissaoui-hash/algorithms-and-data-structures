db54d5fdd1018477c3452ed41aee823418958132
Sahar AISSAOUI <sahar.aissaoui@yahoo.com>
2026-03-03 14:49:32 +0100

    solving lc_217

---
+++ src/patterns/basic_traversal_and_counting/lc_217_contains_duplicate.py
@@ -0,0 +1,14 @@
+#time Complexity: O(n)
+#Space Complexity: O(n)
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+
+        return False
